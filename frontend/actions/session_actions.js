0017811bbbf8ad6346597709b4f44445d2bcf4b5
App Academy Student <email@email.com>
2022-04-22 16:55:46 -0400

    Create SessionReducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,42 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const REMOVE_CURRENT_USER = 'REMOVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUser = (user) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user
+  }
+}
+
+const logoutCurrentUser = () => {
+  return {
+    type: REMOVE_CURRENT_USER
+  }
+}
+
+const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  }
+}
+
+export const login = (formUser) => dispatch => {
+  return SessionAPIUtil.login(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail((errors) => dispatch(receiveErrors(errors.responseJSON)))
+}
+
+export const logout = () => dispatch => {
+  return SessionAPIUtil.logout()
+    .then(() => dispatch(logoutCurrentUser()))
+    .fail((errors) => dispatch(receiveErrors(errors.responseJSON)))
+}
+
+export const signup = (formUser) => dispatch => {
+  return SessionAPIUtil.signup(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail((errors) => dispatch(receiveErrors(errors.responseJSON)))
+}
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { signup, login, logout } from './util/session_api_util'
 document.addEventListener("DOMContentLoaded", () => {
 
   const root = document.getElementById('root');
 
+  window.user = { email: "TestUser1", display_name: "testuser", password: "password"}
+  window.login = login
+  window.signup = signup
+  window.logout = logout
   ReactDOM.render(<h1>Relay Clone</h1>, root);
 });
