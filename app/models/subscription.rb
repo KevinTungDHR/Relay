903f90ff609812a93e02625393a397300946a35b
kevin <kevinkevintung@gmail.com>
2022-04-24 19:12:17 -0400

    Added method to ensure url on workspace creation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,6 +3,7 @@ class Subscription < ApplicationRecord
   validates :pending, :signed_in, inclusion: { in: [true, false] }
   
   belongs_to :subscribeable, polymorphic: true
+  
   belongs_to :user,
     foreign_key: :user_id,
     class_name: :User
--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,6 +1,11 @@
 class Workspace < ApplicationRecord
   validates :name, :url, presence: true
-  
+
+  before_save :add_owner_as_member
+  # must do before save and not after_initialize or else you get a weird error about associationTypeMismatch: user execpted, got nil.
+
+  after_initialize :ensure_unique_url
+
   belongs_to :owner,
     foreign_key: :owner_id,
     class_name: :User
@@ -10,4 +15,15 @@ class Workspace < ApplicationRecord
   has_many :members,
     through: :subscriptions,
     source: :user
+
+  def add_owner_as_member
+    self.members << self.owner if self.new_record?
+  end
+
+  def ensure_unique_url
+    self.url = loop do
+      new_url = "#{self.name}-#{SecureRandom.urlsafe_base64(5)}.relay.herokuapp"
+      break new_url unless self.class.exists?(url: new_url)
+    end
+  end
 end
