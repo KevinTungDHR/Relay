f14e8daffb0b29d9e1b8659781f43d4a36f6b177
kevin <kevinkevintung@gmail.com>
2022-04-26 14:04:50 -0400

    Finish footer

--- frontend/components/main_site/footer/footer.jsx
+++ frontend/components/main_site/footer/footer.jsx
@@ -7,7 +7,18 @@ class Footer extends React.Component {
 
   render(){
     return(
-      <h1 className='footer'>Footer</h1>
+      <section className='footer'>
+        <section className='curved-purple-bg'>
+          <div className='footer-content larger'>
+            <h1>Welcome to where the future works</h1>
+          </div>
+        </section>
+
+        <section className='footer-personal-links'>
+          <a href="https://github.com/KevinTungDHR/Relay" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://www.linkedin.com/in/kevintungmedia/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </section>
+      </section>
     )
   }
 }
