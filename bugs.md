1a65f00769cfa052f37ce2f6c6f6451fcbc4fc5a
kevin <kevinkevintung@gmail.com>
2022-04-26 22:01:31 -0400

    Added draggable windows for chat client, need to create minimums and conditionals for when profile is closed

--- bugs.md
+++ bugs.md
@@ -5,3 +5,8 @@ You must add:
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 componentDidUpdate won't get called when you use Two containers for one presentational component. It is mounting a new component not re-rendering the old one even though they both use the same presentational component.
+
+css bugs:
+
+Always test what you are changing. Set the background color and try something out first
+I kept trying to change a workspace item but i had mispelled it workplace. 30 minutes wasted.
--- frontend/components/chat_client/chat_client.jsx
+++ frontend/components/chat_client/chat_client.jsx
@@ -3,27 +3,85 @@ import React from 'react';
 class ChatClient extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      leftDragging: false,
+      rightDragging: false
+    }
+
+    this.startDrag = this.startDrag.bind(this);
+    this.onDrag = this.onDrag.bind(this);
+    this.endDrag = this.endDrag.bind(this)
+  }
+
+  displayCursor(cursorType){
+    const view = document.querySelector(".client-grid")
+    view.style.cursor = cursorType
+  }
+
+  startDrag(e){
+    console.log(`${e.target.name} true`)
+    this.setState({
+      [e.target.id]: true
+    })
+
+    this.displayCursor("ew-resize")
+  }
+
+  endDrag(e){
+    this.setState({
+      leftDragging: false,
+      rightDragging: false
+    })
+
+    this.displayCursor("auto")
+  }
+
+  onDrag(e){
+    const { leftDragging, rightDragging } = this.state
+
+    if (leftDragging || rightDragging) {
+      console.log("dragging")
+      e.preventDefault();
+      const view = document.querySelector(".client-grid");
+      const sidebar = document.querySelector(".c-workspace-sidebar");
+      const profile = document.querySelector(".c-workspace-profile");
+      const sideBarWidth = leftDragging ? e.clientX : sidebar.clientWidth
+      const profileWidth = rightDragging ? view.clientWidth - e.clientX : profile.clientWidth
+
+      const cols = [
+        sideBarWidth, 
+        3, 
+        view.clientWidth - 6 - sideBarWidth - profileWidth,
+        3,
+        profileWidth
+      ]
+
+      const newTemplate = cols.map(col => col.toString() + "px").join(" ");
+      console.log(newTemplate)
+      view.style.gridTemplateColumns = newTemplate;
+    }
   }
 
   render(){
     return(
-      <div className='client-container'>
-        <div className='client-grid'>
+      <div className='client-container' onMouseUp={this.endDrag}>
+        <div className='client-grid' onMouseMove={this.onDrag}>
+
           <nav className='client-top-nav'>
-
           </nav>
-          <div className='c-workspace-layout'>
-            <section className='c-workspace-sidebar'>
 
-            </section>
-            <div className='client-primary-view'>
+          <section className='c-workspace-sidebar'>
+          </section>
 
-            </div>
-          </div>
+          <div id="leftDragging" className='left-dragbar' onMouseDown={this.startDrag}></div>
+          
+          <div className='client-primary-view'></div>
+
+          <div id="rightDragging" className='right-dragbar' onMouseDown={this.startDrag}></div>
 
           <section className='c-workspace-profile'>
-
           </section>
+
         </div>
       </div>
     )
