f378949b2d85b3e646b57aa171b7caa13d0a5f4a
kevin <kevinkevintung@gmail.com>
2022-04-27 21:13:10 -0400

    Fixing flicker bug

--- frontend/components/main_site/content/splash_page/splash_page.jsx
+++ frontend/components/main_site/content/splash_page/splash_page.jsx
@@ -2,42 +2,67 @@ import React from 'react';
 import GetStartedLanding from './get_started_landing';
 import LoggedinContent from './loggedin_content';
 import LoggedoutContent from './loggedout_content';
+
 class SplashPage extends React.Component {
   constructor(props){
     super(props);
     this.demoLogin = this.demoLogin.bind(this);
+
+    this.state = { isFetched: false }
   }
 
-  // componentDidUpdate(prevProps){
-  //   if(this.props.currentUser && prevProps !== this.props){
-  //     this.props.fetchSignedinWorkspaces()
-  //   }
-  // }
+  // Handles refresh of page
+
+  componentDidMount(){
+    if (!this.props.currentUser){
+      this.setState({ isFetched: true })
+    } else {
+      this.props.fetchSignedinWorkspaces().then(() => {
+        this.setState({ isFetched: true })
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps){
+    console.log(this.state.isFetched)
+
+    const { currentUser } = this.props;
+    if (
+      currentUser && 
+      prevProps.currentUser !== currentUser
+    ){
+      this.props.fetchSignedinWorkspaces()
+        .then(()=>this.setState({isFetched: true}))
+    }
+  }
 
   demoLogin(e){
     e.preventDefault()
-    this.props.login({
-      email: "DemoUser@gmail.com",
-      password: "demouserpassword"
-    })
+    this.setState({ isFetched: false }, () => {
+      this.props.login({
+        email: "DemoUser@gmail.com",
+        password: "demouserpassword"
+      })
+      }
+    )
   }
 
   renderContent(){
+    if (!this.state.isFetched) {
+      return null;
+    }
     const { currentUser, workspaces } = this.props
+
     if (currentUser && workspaces.length > 0){
       return <LoggedinContent currentUser={currentUser} workspaces={workspaces}/>
-    } else if (currentUser){
+    } else if (currentUser) {
       return <GetStartedLanding />
     } else {
       return <LoggedoutContent demoLogin={this.demoLogin}/>
-
     }
   }
 
   render(){
-    // Why doesn't an if statement work here? It doesn't render correctly
-    // const content = currentUser ? 
-    //   <LoggedinContent currentUser={currentUser} workplaces={workplaces}/> : <LoggedoutContent demoLogin={this.demoLogin}/>
     return(
       <div>
         {this.renderContent()}
