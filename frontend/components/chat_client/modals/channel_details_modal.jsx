99fbc61981dc2a0e29af14fec2e39832e5c03f14
kevin <kevinkevintung@gmail.com>
2022-06-20 02:09:30 -0400

    Add people to channels detail modal working

--- frontend/components/chat_client/modals/channel_details_modal.jsx
+++ frontend/components/chat_client/modals/channel_details_modal.jsx
@@ -9,15 +9,17 @@ import { BiSearch } from 'react-icons/bi';
 import { fetchSearchMembers } from '../../../util/search_util';
 import { IoCloseCircle } from 'react-icons/io5';
 import { AiOutlineUserAdd } from 'react-icons/ai';
+import AddPeopleOncreateModalContainer from './channel_modals/add_people_oncreate_modal_container';
 class ChannelDetails extends React.Component {
   constructor(props){
     super(props);
 
-    this.state = { editModalOpen: false, modalName: null, tab: this.props.modal.tab, query: "", searchUsers: [] }
+    this.state = { addChannelMembersModalOpen: false, editModalOpen: false, modalName: null, tab: this.props.modal.tab, query: "", searchUsers: [] }
     this.hideNestedModal = this.hideNestedModal.bind(this);
     this.handleLeave = this.handleLeave.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.openAddChannelMembers = this.openAddChannelMembers.bind(this);
   }
 
 
@@ -191,6 +193,7 @@ class ChannelDetails extends React.Component {
     const channel = this.props.channels[parseInt(channelId)]
     return(
       <div className={`dark-modal modal`}>
+        {this.state.addChannelMembersModalOpen && <AddPeopleOncreateModalContainer />}
         <div className={tab === 1 ? 'channel-details-modal-content' : 'channel-details-modal-content  modal-bg-white'}>
           <header className='channel-details-modal-header'>
             <div className="channel-details-modal-title">
@@ -278,12 +281,12 @@ class ChannelDetails extends React.Component {
               </div>
               <div className='channel-details-user-item-container' onClick={this.openAddChannelMembers}>
                 <div className='details-member-profile-icon-add-container'>
-                    <AiOutlineUserAdd className='details-member-profile-icon-add'/>
-                  </div>
-                  <div className='details-member-user-item-displayname'>
-                    <div>Add people</div>
-                  </div>
+                  <AiOutlineUserAdd className='details-member-profile-icon-add'/>
                 </div>
+                <div className='details-member-user-item-displayname'>
+                  <div>Add people</div>
+                </div>
+              </div>
               {this.renderSearch()}
             </div>
           }
