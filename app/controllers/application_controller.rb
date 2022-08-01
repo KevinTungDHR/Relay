f44743312a503b300a2f30efa9526febf819a6ab
kevin <kevinkevintung@gmail.com>
2022-08-01 08:20:21 -0400

    Change redis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   # MUST TAKE THIS OUT BEFORE PRODUCTION
   
   helper_method :current_user, :logged_in?
