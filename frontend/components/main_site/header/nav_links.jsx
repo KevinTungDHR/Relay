eff9ce11e262968fcf4418d1eafec77cb978ad2f
kevin <kevinkevintung@gmail.com>
2022-08-01 08:24:33 -0400

    Update links

--- frontend/components/main_site/header/nav_links.jsx
+++ frontend/components/main_site/header/nav_links.jsx
@@ -13,10 +13,10 @@ class NavLinks extends React.Component {
     return (
       <nav className='header-nav-link-container' >
         <div className={`business-nav-links ${this.props.whiteLinks}`}>
-          <a href='#'>Portfolio</a>
+          <a href='http://www.kevintung.tech/' rel='noreferrer' target="_blank">Portfolio</a>
           <a href='https://www.linkedin.com/in/kevintungdev/' rel='noreferrer' target="_blank">LinkedIn</a>
           <a href='https://github.com/KevinTungDHR' rel='noreferrer' target="_blank">GitHub</a>
-          <a href='#'>AngelList</a>
+          <a href='https://angel.co/u/kevin-tung-3' rel='noreferrer' target="_blank">AngelList</a>
         </div>
         <SessionLinksContainer />
       </nav>
